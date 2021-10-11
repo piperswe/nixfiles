f603d4814594411268523f8252cf828d2a2e2c50
Piper McCorkle <contact@piperswe.me>
2021-10-11 01:36:46 -0500

    hydra: set buildbox user to root

--- nixosConfigurations/hydra.nix
+++ nixosConfigurations/hydra.nix
@@ -75,6 +75,7 @@ nixpkgs.lib.nixosSystem {
           Host aarch64-buildbox
           Hostname 192.168.0.132
           Port 22
+          User root
         '';
 
         services.openssh.knownHosts = {
