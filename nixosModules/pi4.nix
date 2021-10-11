9a5a9b9e6eb258ff51a354e34baf7762918d451d
Piper McCorkle <contact@piperswe.me>
2021-10-10 23:57:09 -0500

    aarch64-buildbox: use a simpler kernel

--- nixosModules/pi4.nix
+++ nixosModules/pi4.nix
@@ -1,12 +1,53 @@
 context:
-{ pkgs, modulesPath, ... }:
+{ pkgs, lib, modulesPath, ... }:
 {
   imports = [ "${modulesPath}/installer/sd-card/sd-image-aarch64.nix" ];
 
   boot = {
     kernelPackages = pkgs.linuxPackages_rpi4;
     tmpOnTmpfs = true;
-    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+    initrd.availableKernelModules = lib.mkForce [
+      "uas"
+      "sdhci_pci"
+      "simplefb"
+      "vc4"
+      "pcie-brcmstb"
+      "xhci-pci-renesas"
+      "usbhid"
+      "usb_storage"
+      "md_mod"
+      "raid0"
+      "raid1"
+      "raid10"
+      "raid456"
+      "ext2"
+      "ext4"
+      "ahci"
+      "sata_nv"
+      "sata_via"
+      "sata_sis"
+      "sata_uli"
+      "ata_piix"
+      "pata_marvell"
+      "sd_mod"
+      "sr_mod"
+      "mmc_block"
+      "uhci_hcd"
+      "ehci_hcd"
+      "ehci_pci"
+      "ohci_hcd"
+      "ohci_pci"
+      "xhci_hcd"
+      "xhci_pci"
+      "usbhid"
+      "hid_generic"
+      "hid_lenovo"
+      "hid_apple"
+      "hid_roccat"
+      "hid_logitech_hidpp"
+      "hid_logitech_dj"
+      "hid_microsoft"
+    ];
     loader.raspberryPi = {
       enable = true;
       version = 4;
