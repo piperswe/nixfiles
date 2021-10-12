e3e140207bf6b07953040c6df5b72f955fd8a793
Piper McCorkle <contact@piperswe.me>
2021-10-11 20:09:30 -0500

    buildbox: remove special bits for hqr user

--- nixosModules/buildbox.nix
+++ nixosModules/buildbox.nix
@@ -2,11 +2,6 @@
 { ... }:
 {
   users.users.hydra-queue-runner = {
-    description = "SSH user for Hydra";
-    group = "hydra";
-    createHome = true;
-    home = "/var/lib/hydra-queue-runner";
-    isSystemUser = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwdbrLJXDY1n34JmD8DvnFANVHWHHOg64tPaz2owomo root@hydra"
     ];
