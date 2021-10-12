14b54ac76fd0e0f7088037b61f852545cf9dc09d
Piper McCorkle <contact@piperswe.me>
2021-10-11 20:58:33 -0500

    nixpkgs-fmt: more verbosity

--- checks/nixpkgs-fmt.nix
+++ checks/nixpkgs-fmt.nix
@@ -3,7 +3,17 @@ system:
 let
   pkgs = nixpkgs.legacyPackages.${system};
 in
-pkgs.runCommand "nixpkgs-fmt" { } ''
-  ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${../.}/{,**/}*.nix
-  echo ok > $out
-''
+pkgs.runCommand "nixpkgs-fmt"
+{
+  flake = ../.;
+}
+  ''
+    echo checking through nixpkgs-fmt:
+    files=$(ls $flake/{,**/}*.nix)
+    for file in $files
+    do
+      echo ' - '$file
+    done
+    ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check $files
+    echo ok > $out
+  ''
