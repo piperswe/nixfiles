3be0fce110f64334d39a177a13b647dec771e5e7
Piper McCorkle <contact@piperswe.me>
2021-10-12 23:39:49 -0500

    bootstrap-tools: build for all linux systems

--- hydraJobs/bootstrap-tools.nix
+++ hydraJobs/bootstrap-tools.nix
@@ -1,15 +1,23 @@
-{ lib, ... }:
+{ lib, nixpkgs, ... }:
 let
-  make-bootstrap-tools-cross = system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix" { system = "x86_64-linux"; };
-  make-bootstrap-tools = system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools.nix" { inherit system; };
-  cross-tools = arch: (make-bootstrap-tools-cross "${arch}-linux").${arch}.dist;
+  make-bootstrap-tools-cross = from: system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix" { system = from; };
+  make-bootstrap-tools = system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools.nix" { localSystem = system; };
+  cross-tools = arch: (make-bootstrap-tools-cross "x86_64-linux" "${arch}-linux").${arch}.dist;
   native-tools = arch: (make-bootstrap-tools "${arch}-linux").dist;
   tools = arch: {
-    cross = cross-tools arch;
     native = native-tools arch;
-  };
+  } // (nixpkgs.lib.optionalAttrs (arch != "x86_64" && arch != "i386") {
+    cross = cross-tools arch;
+  });
 in
-{
-  powerpc64le-linux = tools "powerpc64le";
-  sparc64-linux = tools "sparc64";
-}
+builtins.listToAttrs
+  (builtins.map
+    (system:
+      {
+        name = system;
+        value = tools (nixpkgs.lib.removeSuffix "-linux" system);
+      }
+    )
+    (builtins.filter
+      (x: (nixpkgs.lib.hasSuffix "-linux" x))
+      lib.supported-platforms.hydra))
