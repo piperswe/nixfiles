de75b58846df44c629fc48a644999668f37147a2
Piper McCorkle <contact@piperswe.me>
2021-10-12 23:33:56 -0500

    Build cross and native bootstrap tools

--- hydraJobs/bootstrap-tools.nix
+++ hydraJobs/bootstrap-tools.nix
@@ -1,7 +1,13 @@
 { lib, ... }:
 let
   make-bootstrap-tools-cross = system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix" { system = "x86_64-linux"; };
-  tools = arch: (make-bootstrap-tools-cross "${arch}-linux").${arch}.dist;
+  make-bootstrap-tools = system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools.nix" { inherit system; };
+  cross-tools = arch: (make-bootstrap-tools-cross "${arch}-linux").${arch}.dist;
+  native-tools = arch: (make-bootstrap-tools "${arch}-linux").dist;
+  tools = arch: {
+    cross = cross-tools arch;
+    native = native-tools arch;
+  };
 in
 {
   powerpc64le-linux = tools "powerpc64le";
