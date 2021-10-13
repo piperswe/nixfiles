2a346958fe0e4e7fbb95ff3a39fcf0bb3a7293ca
Piper McCorkle <contact@piperswe.me>
2021-10-13 00:45:06 -0500

    hydraJobs: build bootstrap-tools from scratch

--- hydraJobs/bootstrap-tools.nix
+++ hydraJobs/bootstrap-tools.nix
@@ -1,21 +1,14 @@
-{ lib, nixpkgs, ... }:
+{ lib, nixpkgs-piper-bootstrap, ... }:
 let
-  make-bootstrap-tools-cross = from: system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix" { system = from; };
-  make-bootstrap-tools = system: import "${lib.nixpkgs-for-system system}/pkgs/stdenv/linux/make-bootstrap-tools.nix" { localSystem = system; };
-  cross-tools = arch: (make-bootstrap-tools-cross "x86_64-linux" "${arch}-linux").${arch}.dist;
-  native-tools = arch: (make-bootstrap-tools "${arch}-linux").dist;
-  tools = arch: {
-    native = native-tools arch;
-  } // (nixpkgs.lib.optionalAttrs (arch != "x86_64" && arch != "i386") {
-    cross = cross-tools arch;
-  });
+  nixpkgs = nixpkgs-piper-bootstrap;
+  tools = system: import "${nixpkgs}/pkgs/stdenv/linux/scratch" { from = "x86_64-linux"; to = system; };
 in
 builtins.listToAttrs
   (builtins.map
     (system:
       {
         name = system;
-        value = tools (nixpkgs.lib.removeSuffix "-linux" system);
+        value = tools system;
       }
     )
     (builtins.filter
