779ddb8aae7d9dc251362d9c0b2549037eae6ec9
Piper McCorkle <contact@piperswe.me>
2021-10-12 22:29:30 -0500

    bootstrap-tools: fix eval

--- hydraJobs/bootstrap-tools.nix
+++ hydraJobs/bootstrap-tools.nix
@@ -1,6 +1,6 @@
 { nixpkgs-piper-bootstrap, ... }:
 let
-  make-bootstrap-tools-cross = import "${nixpkgs-piper-bootstrap}/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix";
+  make-bootstrap-tools-cross = import "${nixpkgs-piper-bootstrap}/pkgs/stdenv/linux/make-bootstrap-tools-cross.nix" { system = "x86_64-linux"; };
   tools = arch: make-bootstrap-tools-cross.${arch}.dist;
 in
 {
