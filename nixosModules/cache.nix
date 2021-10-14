e753c70d67cc679fe97d95d3bf59d2fa26a1a28d
Piper McCorkle <contact@piperswe.me>
2021-10-14 14:05:57 -0500

    cache: remove armv7 cache

--- nixosModules/cache.nix
+++ nixosModules/cache.nix
@@ -5,12 +5,10 @@
     binaryCaches = [
       "https://nix-cache.piperswe.me"
       "https://nix-community.cachix.org"
-      "https://thefloweringash-armv7.cachix.org"
     ];
     binaryCachePublicKeys = [
       "nix-cache.piperswe.me:4r7vyJJ/0riN8ILB+YhSCnYeynvxOeZXNsPNV4Fn8mE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "thefloweringash-armv7.cachix.org-1:v+5yzBD2odFKeXbmC+OPWVqx4WVoIVO6UXgnSAWFtso="
     ];
   };
 }
