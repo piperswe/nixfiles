65dedc367e954327a3c11024aa599b5ab934b12f
Piper McCorkle <contact@piperswe.me>
2021-10-14 14:18:06 -0500

    hydraJobs/stdenv: build multi-stage

--- hydraJobs/stdenv.nix
+++ hydraJobs/stdenv.nix
@@ -1,6 +1,8 @@
-{ lib, ... }:
+{ nixpkgs-piper-bootstrap, ... }:
 let
-  stdenv = system: (lib.nixpkgs-for-system system).legacyPackages.${system}.stdenv;
+  nixpkgs = nixpkgs-piper-bootstrap;
+  pkgs = system: (import "${nixpkgs}/pkgs/stdenv/linux/scratch" { from = "x86_64-linux"; to = system; }).pkgsWithTools;
+  stdenv = system: (pkgs system).stdenv;
 in
 builtins.listToAttrs
   (builtins.map
