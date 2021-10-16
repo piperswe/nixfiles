0bdcf4c0c3da438a4e795f2b3bae1d9fe8bb9825
Piper McCorkle <contact@piperswe.me>
2021-10-16 01:32:01 -0500

    cache: add haskel

--- nixosModules/cache.nix
+++ nixosModules/cache.nix
@@ -5,10 +5,12 @@
     binaryCaches = [
       "https://nix-cache.piperswe.me"
       "https://nix-community.cachix.org"
+      "https://hydra.iohk.io"
     ];
     binaryCachePublicKeys = [
       "nix-cache.piperswe.me:4r7vyJJ/0riN8ILB+YhSCnYeynvxOeZXNsPNV4Fn8mE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
   };
 }
