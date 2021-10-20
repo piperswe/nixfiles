a19f7f1864191430c10d599048915e6d246e9e54
Piper McCorkle <contact@piperswe.me>
2021-10-20 01:14:54 -0500

    hydra: build x86_64 locally again

--- homeModules/gui.nix
+++ homeModules/gui.nix
@@ -26,13 +26,124 @@ in
           { compatible = x86_64-linux; pkg = steam; }
           { compatible = x86_64-linux; pkg = steam-run; }
           { compatible = x86_64-linux; pkg = yubioath-desktop; }
-          { compatible = builtins.elem stdenv.system [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "armv7l-linux" ]; pkg = vscodium; }
           { compatible = stdenv.isLinux; pkg = openrgb; }
           { compatible = builtins.elem stdenv.system [ "x86_64-darwin" "x86_64-linux" "i686-linux" "aarch64-linux" "aarch64-darwin" ]; pkg = element-desktop; }
           { compatible = stdenv.isLinux; pkg = virt-manager; }
           { compatible = builtins.elem stdenv.system [ "i686-linux" "x86_64-linux" ]; pkg = jetbrains.idea-ultimate; }
-          josm
+          { compatible = builtins.elem stdenv.system [ "x86_64-darwin" "aarch64-darwin" "aarch64-linux" "i686-linux" "x86_64-linux" ]; pkg = dhall-lsp-server; }
+          { compatible = builtins.elem stdenv.system [ "x86_64-darwin" "aarch64-darwin" "aarch64-linux" "i686-linux" "x86_64-linux" ]; pkg = josm; }
         ];
     allowUnfreePackages = [ "1password" "plexamp" "steam" "steam-original" "steam-runtime" "idea-ultimate" ];
+
+    programs.vscode = lib.mkIf (builtins.elem pkgs.stdenv.system [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "armv7l-linux" ]) {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions;
+        let
+          generic = [
+            editorconfig.editorconfig
+            (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+              mktplcRef = {
+                publisher = "wayou";
+                name = "vscode-todo-highlight";
+                version = "1.0.4";
+                sha256 = "sha256-OdPcQNZv55v2MGkccXD0qmgmGtXnmS6A2VcjY1YuImk=";
+              };
+            })
+          ];
+          rust = [
+            tamasfe.even-better-toml
+            matklad.rust-analyzer
+          ];
+          js = [
+            dbaeumer.vscode-eslint
+            esbenp.prettier-vscode
+            octref.vetur
+          ];
+          ruby = [
+            (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+              mktplcRef = {
+                publisher = "rebornix";
+                name = "Ruby";
+                version = "0.28.1";
+                sha256 = "sha256-HAUdv+2T+neJ5aCGiQ37pCO6x6r57HIUnLm4apg9L50=";
+              };
+            })
+            (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+              mktplcRef = {
+                publisher = "wingrunr21";
+                name = "vscode-ruby";
+                version = "0.28.0";
+                sha256 = "sha256-H3f1+c31x+lgCzhgTb0uLg9Bdn3pZyJGPPwfpCYrS70=";
+              };
+            })
+          ];
+          go = [
+            golang.go
+          ];
+          haskell = [
+            justusadam.language-haskell
+            pkgs.vscode-extensions.haskell.haskell
+          ];
+          purescript = with pkgs.vscode-utils; [
+            (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+              mktplcRef = {
+                publisher = "nwolverson";
+                name = "language-purescript";
+                version = "0.2.5";
+                sha256 = "sha256-7o8pGMw66izeEz9INmG0+OT0gOysSOr4zUYgpBS++b0=";
+              };
+            })
+            (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+              mktplcRef = {
+                publisher = "nwolverson";
+                name = "ide-purescript";
+                version = "0.25.2";
+                sha256 = "sha256-k+Dokvchw49Rez5jB+eYZUsQENw2VrDXVSXrwJ4fUYA=";
+              };
+            })
+          ];
+          terraform = [
+            hashicorp.terraform
+          ];
+          java = [
+            (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+              mktplcRef = {
+                publisher = "wmanth";
+                name = "jar-viewer";
+                version = "1.1.0";
+                sha256 = "sha256-11BgMivgWuOUPItIxTRdpPy3iy3okW08KF0e55Lcch4=";
+              };
+            })
+          ];
+          nix = [
+            arrterian.nix-env-selector
+            jnoortheen.nix-ide
+          ];
+          protobuf = [
+            zxh404.vscode-proto3
+          ];
+          dhall = [
+            pkgs.vscode-extensions.dhall.dhall-lang
+            pkgs.vscode-extensions.dhall.vscode-dhall-lsp-server
+          ];
+          misc-devops = [
+            ms-azuretools.vscode-docker
+          ];
+        in
+        generic
+        ++ rust
+        ++ js
+        ++ ruby
+        ++ go
+        ++ haskell
+        ++ purescript
+        ++ terraform
+        ++ java
+        ++ nix
+        ++ protobuf
+        ++ dhall
+        ++ misc-devops;
+    };
   };
 }
