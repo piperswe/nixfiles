30033e989130037aec94dfeb30472625542a2239
Piper McCorkle <contact@piperswe.me>
2021-12-15 19:50:34 -0600

    ssh-keys: factor out

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,7 @@
 context:
 {
   nixpkgs-for-system = import ./nixpkgs-for-system.nix context;
+  ssh-keys = import ./ssh-keys.nix context;
   supported-platforms = import ./supported-platforms.nix context;
   universal-packages = import ./universal-packages.nix context;
 }
