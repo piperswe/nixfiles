dd58b487e1690567da1bc119597c1b823eaae147
Piper McCorkle <piper@lutris.engineering>
2022-05-18 16:41:47 -0500

    Slim down Hydra jobs

--- hydraJobs/default.nix
+++ hydraJobs/default.nix
@@ -1,6 +1,6 @@
 context:
 {
-  bootstrap-tools = import ./bootstrap-tools.nix context;
-  cross-stdenv = import ./cross-stdenv.nix context;
-  stdenv = import ./stdenv.nix context;
+  # bootstrap-tools = import ./bootstrap-tools.nix context;
+  # cross-stdenv = import ./cross-stdenv.nix context;
+  # stdenv = import ./stdenv.nix context;
 }
